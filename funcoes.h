729c84f1651a7588e88b25b1bcb83b9c2b35598f
AugustoPAraujo <49997858+AugustoPAraujo@users.noreply.github.com>
2019-06-04 19:40:53 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -7,6 +7,13 @@ using std::string;
 //Transforma a palavra toda em minusculo e retira caracter especial
 void minusculo (string& s);
 
-
+class Dicionario{
+    public:
+		Dicionario();
+	private:
+		set< string > st;
+		map< set< string >, int> id;
+		map< string, map< set< string >, int> > mp;
+};
 
 #endif //FUNCOES_H
