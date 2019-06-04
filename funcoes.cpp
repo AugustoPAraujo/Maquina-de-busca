d7fc52dd66637e75737ede6a4070d67b686600bc
AugustoPAraujo <49997858+AugustoPAraujo@users.noreply.github.com>
2019-06-04 19:37:31 -0300

    Update funcoes.cpp

--- funcoes.cpp
+++ funcoes.cpp
@@ -10,3 +10,31 @@ void minusculo (string& s){
     }
   }
 }
+
+//   mp   palavra     id         st      tf                //
+//   |     |            |         |      |                 //
+//	map<string,       map< set<string>, int >     -> mapa  //
+//       |                      |                          //
+//     palavra             doc(chave)                      //
+                                                        
+Dicionario::Dicionario(string doc, string palavra){
+	if(mp.count(palavra) == 0){ //se não houver a palavra no mapa
+		st.insert(doc);
+		set<string>::iterator i=st.begin();
+		id[st[*i]] = 1;
+		mp[palavra] = id;
+	}else{
+		if(!st.count(doc)){
+			st.insert(doc);
+			set<string>::iterator i=st.begin();
+			id[st[*i]] = 1;
+		}else{
+			for(set<string>::iterator i=st.begin(); i != st.end(); i++){
+				if(*i == doc){
+					id[st[*i]] += 1;
+				}
+			}
+		}
+		mp[palavra] = id;
+	}
+}
